af63b5608aaf6b432dae9eb98435e238d0c4c8fb
T.J <t.ruggles@live.ca>
2018-10-15 01:32:12 -0400

    Initial files

---
+++ public/scripts/app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+console.log('App.js is running');
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of your computer!',
+  options: ['One', 'Two']
+};
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  true && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item 1'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item 2'
+    )
+  )
+);
+
+var user = {
+  name: 'T.J.',
+  age: 27,
+  location: 'Ottawa, Canada'
+};
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+var userName = 'Mike';
+var userAge = '69';
+var userLocation = 'Toronto, ON, Canada';
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
